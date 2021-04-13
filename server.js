6b8afcc61cbcf965813be6f001894d7285a3c156
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-12 21:01:45 -0500

    activity

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Dependencies
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -8,3 +9,31 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const waitingList = [];
+const table = [{
+    name: 'Bob',
+    phone: '555-5555',
+    email: 'bob@bob.com',
+    uniqueID: '1234'
+}];
+
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/api/tables', (req, res) => (res.json({table: table, waitingList: waitingList})));
+// app.get('/api/tables', (req, res) => res.json(path.join(__dirname, 'tables.html')));
+
+app.post('/api/reservations', (req, res) => {
+    const newRequest = req.body;
+    if(table.length < 5 ) {
+        table.push(newRequest)
+    }
+    else {
+        waitingList.push(newRequest);
+    }
+    return res.json(newRequest)
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
